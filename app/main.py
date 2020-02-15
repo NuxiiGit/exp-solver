fcfc79ae6a19779a41e40b575b224e473ae90bbd
NuxiiGit <nuxii09@gmail.com>
2020-02-15 01:00:28 +0000

    Tested command line arguments. Removed batch run script.

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-print("Hello!")
+import sys
+
+argument = sys.argv
+argument_count = len(argument)
+
+if argument_count < 1:
+    print("invalid number of arguments")
+else:
+    for arg in argument:
+        print("arg: ", arg)
