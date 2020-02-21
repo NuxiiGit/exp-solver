214148d4c94d3940905c75b7161d145f6cf85e8c
NuxiiGit <nuxii09@gmail.com>
2020-02-21 03:16:55 +0000

    Updated .gitignore

--- app/main.py
+++ app/main.py
@@ -1,10 +1,4 @@
-import sys
+from test import *
 
-argument = sys.argv
-argument_count = len(argument)
-
-if argument_count < 1:
-    print("invalid number of arguments")
-else:
-    for arg in argument:
-        print("arg: ", arg)
+print("go")
+stuff()
