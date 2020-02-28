906e096af52c48ca1ab8ef0436357f00bc854fd8
NuxiiGit <nuxii09@gmail.com>
2020-02-28 01:26:26 +0000

    Changed up the main.

--- app/main.py
+++ app/main.py
@@ -3,8 +3,14 @@ from lexer import Lexer
 import sys
 
 argument = sys.argv
-del argument[0]
-src = " ".join(argument)
-lexer = iter(Lexer(src))
-for token in lexer:
-    print(token)
+del argument[0] # don't need to know about the working directory
+argument_count = len(argument)
+if argument_count == 0:
+    print("invalid argument count")
+    print("please pass the expression as a string, e.g.")
+    print("  \"2a + 5b\"")
+else:
+    src = argument[0]
+    lexer = iter(Lexer(src))
+    for token in lexer:
+        print(token)
