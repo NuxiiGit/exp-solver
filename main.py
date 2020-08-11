d65364c51d98c7363c1fc2d2156e8e70d630c592
NuxiiGit <nuxii09@gmail.com>
2020-08-12 00:33:46 +0100

    bweh

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if argument_count == 0:
 else:
     src = argument[0]
     expr = parse.Parser(src).parse()
-    evaluator = evaluate.Evaluator()
-    value = evaluator.evaluate(expr)
+    #evaluator = evaluate.Evaluator()
+    value = evaluate.evaluate(expr, { "x" : 0 })
     print("expression: " + str(expr))
     print("value:      " + str(value))
