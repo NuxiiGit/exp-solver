0dc981d154282fd769f0a02c8fea6f382a4be3bd
NuxiiGit <nuxii09@gmail.com>
2020-08-14 21:41:17 +0100

    updated main to include options

--- main.py
+++ main.py
@@ -2,15 +2,22 @@ import parse
 import solve
 import sys
 
-argument = sys.argv
-del argument[0] # don't need to know about the working directory
-argument_count = len(argument)
-if argument_count == 0:
+args = sys.argv
+del args[0] # don't want working directory
+count = len(args)
+if count == 0:
+    print("usage")
+elif count < 2:
     print("invalid argument count")
-    print("please pass the expression as a string, e.g.")
-    print("  \"2 + 5\"")
 else:
-    src = argument[0]
+    kind = args[0]
+    src = args[1]
+    options = args[1 :]
     expr = parse.Parser(src).parse()
-    print("expression: %s" % expr)
-    print("solution:   %s" % solve.hillclimb(expr, "x", start=0))
+    print("--> %s" % expr)
+    if kind == "hillclimb":
+        print("attempting hillclimb...")
+        solution = solve.hillclimb(expr, "x", start=0)
+        print("  solution:   %s" % solution)
+    else:
+        print("unknown solver option")
